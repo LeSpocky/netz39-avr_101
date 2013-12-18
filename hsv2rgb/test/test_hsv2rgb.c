8733b3fe8572b799987f2e96f962fa7ddcb82a52
Alexander Dahl <alex@netz39.de>
2013-12-18 19:17:05 +0100

    prepare for a new hsv2rgb implementation
    this is intended for microcontroller use, so it will be optimized for a 8 bit cpu target

---
+++ hsv2rgb/test/test_hsv2rgb.c
@@ -0,0 +1,32 @@
+/*******************************************************************//**
+ *	@file	test_hsv2rgb.c
+ *
+ *	@author	Alexander Dahl <post@lespocky.de>
+ *
+ *	@copyright	2013 Alexander Dahl
+ *
+ *	This file is part of hsv2rgb.
+ *
+ *	_hsv2rgb_ is free software: you can redistribute it and/or modify it
+ *	under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	_hsv2rgb_ is distributed in the hope that it will be useful, but
+ *	WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *	General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with _hsv2rgb_. If not, see <http://www.gnu.org/licenses/>.
+ **********************************************************************/
+
+#include <stdlib.h>
+
+#include "hsv2rgb.h"
+
+int main( void ) {
+	return EXIT_FAILURE;
+}
+
+/* vim: set noet sts=0 ts=4 sw=4 sr: */
